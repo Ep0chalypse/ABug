879f98b4d60ac6b30bb0b541c3995dcab0609e47
fake <fake@fake.com>
2018-10-10 13:33:22 -0400

    Initial Commit

---
+++ app/src/main/java/com/fake/testproject/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.fake.testproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.fake.testproject.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
